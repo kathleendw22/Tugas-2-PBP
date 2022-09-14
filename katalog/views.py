055d6c6d19142876188f9e9ccd08747a549579a8
Kathleen Daniella <kathleen.daniella@ui.ac.id>
2022-09-14 10:39:19 +0700

    changes

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_barang': catalog_item,
+        'nama': 'Kathleen',
+        'student_id': '2106637366'
+    }
+    return render(request, "katalog.html", context)
