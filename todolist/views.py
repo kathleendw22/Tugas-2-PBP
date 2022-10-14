e4ce34e01d2283e87760538dd49dca48fec03957
Kathleen Daniella <kathleen.daniella@ui.ac.id>
2022-10-14 16:01:40 +0700

    deploy

--- todolist/views.py
+++ todolist/views.py
@@ -22,7 +22,6 @@ def show_todolist(request):
     context = {
         'list_barang': todolist_item,
         'username': request.user,
-        'last_login': request.COOKIES['last_login'],
     }
     return render(request, "todolist.html", context)
 
@@ -63,7 +62,6 @@ def login_user(request):
         if user is not None:
             login(request, user) # melakukan login terlebih dahulu
             response = HttpResponseRedirect(reverse("todolist:show_todolist")) # membuat response
-            response.set_cookie('last_login', str(datetime.datetime.now())) # membuat cookie last_login dan menambahkannya ke dalam response
             return response
         else:
             messages.info(request, 'Username atau Password salah!')
@@ -73,7 +71,6 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('todolist:login'))
-    response.delete_cookie('last_login')
     return response
 
 def create_task(request):
