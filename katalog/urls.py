c608ae0b32c8d10572985a3816080844af01505b
Kathleen Daniella <kathleen.daniella@ui.ac.id>
2022-10-17 22:27:31 +0700

    test

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,9 +1,11 @@
 # TODO: Implement Routings Here
 from django.urls import path
 from katalog.views import show_katalog
+from katalog.views import show_json
 
 app_name = 'katalog'
 
 urlpatterns = [
     path('', show_katalog, name='show_katalog'),
+    path('json/', show_json, name='show_json'),
 ]
--- katalog/views.py
+++ katalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from katalog.models import CatalogItem
+from django.http import HttpResponse
+from django.core import serializers
 
 # TODO: Create your views here.
 def show_katalog(request):
@@ -10,3 +12,7 @@ def show_katalog(request):
         'student_id': '2106637366'
     }
     return render(request, "katalog.html", context)
+
+def show_json(request):
+    catalog_item = CatalogItem.objects.all()
+    return HttpResponse(serializers.serialize("json", catalog_item), content_type="application/json")
